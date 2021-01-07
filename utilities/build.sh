42081b53e6609acb34b84fe5ebfa0331e8b20a07
Tony Kay <tony.g.kay@gmail.com>
2021-01-07 15:32:29 -0700

    Updates for first PoC

--- utilities/build.sh
+++ utilities/build.sh
@@ -1,5 +1,5 @@
 echo Build a demo zip archive
 
 cd ..
-zip -r ./dojo/builds/dojo-multitier.zip ./dojo/README.adoc ./dojo/resources ./dojo/run.sh
+zip -r ./dojo/builds/dojo-multitier.zip ./dojo/README.adoc ./dojo/resources ./dojo/start.sh
 
