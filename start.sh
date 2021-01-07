da2b3e6fad9f5fcec0e85015656e8f265acc15f7
Tony Kay <tony.g.kay@gmail.com>
2021-01-07 14:40:56 -0700

    Enhancements to installation of os in start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 
 _DOJO_HOME=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH=$SELF_PATH/$(basename -- "$0")
 
-detect_os () # For container run-time and installers
+validate_runtime_by_os () # For container run-time and installers
 {
   _PLATFORM=unknown
   _OS_STRING=$(uname)
@@ -12,9 +12,11 @@ detect_os () # For container run-time and installers
   case "$_OS_STRING" in
     Darwin)
       _PLATFORM=mac
+      ensure_mac_container_runtime
       ;;
     Linux)
       _PLATFORM=Linux
+      echo Linux Support via podman coming
       ;;
     *)
       echo Platform is unknown
@@ -23,24 +25,44 @@ detect_os () # For container run-time and installers
   esac
 }
 
-ensure_container_runtime ()
+#ensure_mac_software_installed ()
+#{
+#  _MACOS_VERSION=$(defaults read loginwindow SystemVersionStampAsString)
+#  echo Mac version "$_MACOS_VERSION"
+#}  
+
+ensure_mac_container_runtime ()
 {
   # deal with Mac && Docker
-  echo mac
-  # deal with Linux && Podman
-  # deal with Linux && Docker
+  if [ -f /usr/local/bin/docker-composeid ]
+  then
+    echo docker installed
+  else
+    # mac_version_installer
+    _DOCKER_DESKTOP_MINIMUM="10.14"
+    _MACOS_VERSION=$(defaults read loginwindow SystemVersionStampAsString)
+    echo docker-compose not installed
+    echo You can install this manualy with:
+    echo
+     if [ "$(printf '%s\n' "$_DOCKER_DESKTOP_MINIMUM" "$_MACOS_VERSION" | sort -V | head -n1)" = "$_DOCKER_DESKTOP_MINIMUM" ]; then 
+            echo brew cask install docker
+     else
+            echo brew cask install docker-toolbox
+     fi
+     exit 1
+  fi
 }
-
 
 run_docker_compose_deployment ()
 {
   docker-compose -f ${_DOJO_HOME}/resources/orchestration/docker-compose.yml up -d
 }
 
-
 detect_os
+# ensure_container_runtime
 run_docker_compose_deployment 
-
-echo 
+				 
+echo
 echo "You can access your lab at http://localhost"
 echo
+
