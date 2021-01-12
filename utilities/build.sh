33426b2fe9e8d2c0ab010885f55d29cfcc3257c5
Tony Kay <tony.g.kay@gmail.com>
2021-01-12 10:48:02 -0700

    Updates to build process and new build

--- utilities/build.sh
+++ utilities/build.sh
@@ -2,5 +2,5 @@ echo Build a demo zip archive
 
 cd ..
 rm ./dojo/builds/dojo-multitier.zip
-zip -r ./dojo/builds/dojo-multitier.zip ./dojo/README.adoc ./dojo/resources ./dojo/start.sh
+zip -r ./dojo/builds/dojo-multitier.zip ./dojo/README.adoc ./dojo/resources ./dojo/dojo
 
